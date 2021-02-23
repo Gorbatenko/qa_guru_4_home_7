28dda82a4e0e8612202ae1fdbbc97af3d9530cc6
Gorbatenko Vitaliy <gorvit.dp@gmail.com>
2021-02-23 23:40:05 +0200

    add simple tests

--- src/test/java/qa/guru/SimpleTest.java
+++ src/test/java/qa/guru/SimpleTest.java
@@ -1,5 +1,18 @@
 package qa.guru;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class SimpleTest {
 
+    @Test
+    void simplePassedTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void simpleFailureTest() {
+        assertTrue(false, "failure test is failed. It's Ok.");
+    }
 }
